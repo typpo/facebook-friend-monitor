13aee1459f61fbe06047890c6ce76d3a635505f0
Ian <ian@typpo.us>
2011-03-21 21:50:31 -0400

    added constants

---
+++ constants.py
@@ -0,0 +1,9 @@
+# Activate for local deployment
+DEBUG = True
+
+if DEBUG:
+    FACEBOOK_APP_ID = '183916088320307'
+    FACEBOOK_APP_SECRET = '54eacbbd8cb433b68a67282f8d83fb0a'
+else:
+    FACEBOOK_APP_ID = "172469002787534"
+    FACEBOOK_APP_SECRET = "5e4f10d636ea301cd232df4a758c4fd5"
